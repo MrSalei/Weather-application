1bf92cff985dea09b165dcd7bb639dd62b089638
MrSalei <ilia74311@yandex.by>
2022-07-21 14:14:08 +0300

    basics

--- Weather application/ViewController.swift
+++ Weather application/ViewController.swift
@@ -8,12 +8,27 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var locationLabel: UILabel!
+    @IBOutlet weak var dayLabel: UILabel!
+    @IBOutlet weak var conditionImageView: UIImageView!
+    @IBOutlet weak var conditionLabel: UILabel!
+    @IBOutlet weak var temperatureLabel: UILabel!
+    @IBOutlet weak var backgroundView: UIView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        setGrayGragientBackground()
+    }
+    
+    func setBlueGragientBackground() {
+        backgroundView.backgroundColor = .cyan
+    }
+    
+    func setGrayGragientBackground() {
+        backgroundView.backgroundColor = .darkGray
     }
 
-
 }
-
